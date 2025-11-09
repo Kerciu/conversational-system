abf20a13786c8519443e058c76dcbe1d87eb4c30
Kerciu <kerciuuu@gmail.com>
2025-11-09 01:14:47 +0100

    add env variables to the backend service; mock redis and rabbit services in app tests

--- backend-service/src/test/java/com/conversational/system/application/ApplicationTests.java
+++ backend-service/src/test/java/com/conversational/system/application/ApplicationTests.java
@@ -1,13 +1,38 @@
 package com.conversational.system.application;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @SpringBootTest
 class ApplicationTests {
 
+	@TestConfiguration
+	static class MockBeanConfiguration {
+
+		@Bean
+		@Primary
+		public RabbitTemplate testRabbitTemplate() {
+			return Mockito.mock(RabbitTemplate.class);
+		}
+
+		@Bean
+		@Primary
+		public RedisTemplate<String, Object> testRedisTemplate() {
+			@SuppressWarnings("unchecked")
+			RedisTemplate<String, Object> mockTemplate = (RedisTemplate<String, Object>) Mockito.mock(RedisTemplate.class);
+			return mockTemplate;
+		}
+	}
+
 	@Test
 	void contextLoads() {
+		System.out.println("Context loaded successfully with @Primary @Bean mocks!");
 	}
 
 }
