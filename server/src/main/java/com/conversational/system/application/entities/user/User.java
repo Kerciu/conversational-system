c7e216152151e38f269c4e3912ad8a9e91b23a31
biegaczhanna <biegacz.hanna@gmail.com>
2025-11-12 13:01:18 +0100

    created a User entity

---
+++ server/src/main/java/com/conversational/system/application/entities/user/User.java
@@ -0,0 +1,47 @@
+package com.conversational.system.application.entities.user;
+
+import java.time.LocalDateTime;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "app_user")
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+    
+    @Column(name = "username", unique = true, nullable = false)
+    private String username;
+
+    @Column(name = "password_hash")
+    private String passwordHash;
+
+    @Column(name = "creation_date")
+    private LocalDateTime creationDate;
+
+    @Column(name = "is_verified", nullable = false)
+    private boolean isVerified;
+    
+    public User(String email, String username, String passwordHash) {
+        this.email = email;
+        this.username = username;
+        this.passwordHash = passwordHash;
+        this.creationDate = LocalDateTime.now();
+        this.isVerified = true; // will be initialized to false when email verification is implemented
+    }
+}
