6349899b735bf74d278c0858563bc50b5bc36163
hbiegacz <biegacz.hanna@gmail.com>
2025-11-18 20:34:59 +0100

    added rest template

--- backend-service/src/main/java/com/conversational/system/application/config/SecurityConfig.java
+++ backend-service/src/main/java/com/conversational/system/application/config/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -38,6 +39,10 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean 
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
