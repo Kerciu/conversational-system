f6ed948333f0fec775c30252543e9130c56233c4
hbiegacz <biegacz.hanna@gmail.com>
2025-12-10 15:19:46 +0100

    new entity for chat messages

---
+++ backend-service/src/main/java/com/conversational/system/application/entities/message/Message.java
@@ -0,0 +1,58 @@
+package com.conversational.system.application.entities.message;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import com.conversational.system.application.entities.conversation.Conversation;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+enum Sender {
+    USER,
+    MODELER,
+    CODER,
+    VISUALIZER,
+}
+
+@Entity
+@Getter
+@Setter
+@Table(name = "message")
+@NoArgsConstructor
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @CreationTimestamp
+    @Column(name = "timestamp", nullable = false)
+    private LocalDateTime timestamp;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "conversation_id", nullable = false)
+    private Conversation conversation;
+
+    @Column(name = "sender", nullable = false)
+    private Sender sender;
+
+    public Message(String content, Conversation conversation, Sender sender) {
+        this.content = content;
+        this.conversation = conversation;
+        this.sender = sender;
+    }
+}
---
+++ backend-service/src/main/java/com/conversational/system/application/entities/message/MessageRepository.java
@@ -0,0 +1,15 @@
+package com.conversational.system.application.entities.message;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface MessageRepository extends JpaRepository<Message, Integer> {
+    public Optional<Message> findById(Integer id);
+
+    public List<Message> findByConversationId(Integer conversationId);
+
+}
